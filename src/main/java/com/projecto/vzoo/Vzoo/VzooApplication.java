945ccf8629b0caab5856ecfcba1c51f7a3f48c41
Fernando Marta <fernando.marta@outlook.com>
2020-05-16 16:43:34 +0100

    Initial commit

---
+++ src/main/java/com/projecto/vzoo/Vzoo/VzooApplication.java
@@ -0,0 +1,13 @@
+package com.projecto.vzoo.Vzoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VzooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VzooApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/projecto/vzoo/Vzoo/VzooApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projecto.vzoo.Vzoo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VzooApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
